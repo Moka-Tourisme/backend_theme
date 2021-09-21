a20d07bcfb711bb3cf719339b60f2a060769afe1
Grégory Schreiner <gregory@mokatourisme.fr>
2021-09-21 13:52:25 +0200

    Change BG-image

--- backend_theme_v12/controllers/main.py
+++ backend_theme_v12/controllers/main.py
@@ -6,7 +6,7 @@ import base64
 from odoo.http import Controller, request, route
 from werkzeug.utils import redirect
 
-DEFAULT_IMAGE = '/backend_theme_v12/static/src/img/material-background.jpg'
+DEFAULT_IMAGE = '/backend_theme_v12/static/src/img/Moka-background.jpg'
 
 class DasboardBackground(Controller):
 
